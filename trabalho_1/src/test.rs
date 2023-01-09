71d87fcb715805b5fdeb7e96ce024e45797a3367
JoaoCosme <cosmejoaopedro@protonmail.com>
2023-01-09 14:26:08 -0300

    :memo: adiciona novo relatorio

--- trabalho_1/src/test.rs
+++
@@ -1,8 +0,0 @@
-use crate::matrix_ops::apply_kernel;
-
-#[test]
-pub(crate) fn test_apply_matrix() {
-    let laplacian = [[0.0, -1.0, 0.0], [-1.0, 4.0, -1.0], [0.0, -1.0, 0.0]];
-    let matrix = [[0, 1, 10], [0, 10, 5], [0, 1, 2]];
-    assert_eq!(apply_kernel(laplacian, matrix), 33);
-}
