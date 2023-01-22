6b13eb5d33a2a21ce9c916c8af2716d69c61612c
JoaoCosme <cosmejoaopedro@protonmail.com>
2023-01-22 16:24:13 -0300

    add base loop

--- trabalho-1-p3/src/main.rs
+++ trabalho-1-p3/src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use opencv::{highgui, prelude::*, videoio, Result};
+
+fn main() -> Result<()> {
+    let mut cam =
+        videoio::VideoCapture::new(0, videoio::CAP_ANY).expect("Should be able to open camera!");
+
+    highgui::named_window("Window", highgui::WINDOW_FULLSCREEN)
+        .expect("Should be able to open window!");
+
+    let mut frame = Mat::default();
+
+    loop {
+        cam.read(&mut frame)
+            .expect("Should be able to acquire new frame!");
+
+        highgui::imshow("window", &frame)?;
+        let key = highgui::wait_key(1)?;
+        if key == 113 {
+            break;
+        }
+    }
+
+    Ok(())
 }
