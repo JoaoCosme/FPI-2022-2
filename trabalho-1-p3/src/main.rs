61da1d53d087adb5894f2f6c012eae881b826bbd
JoaoCosme <cosmejoaopedro@protonmail.com>
2023-01-23 10:54:59 -0300

    :sparkles: add canny

--- trabalho-1-p3/src/main.rs
+++ trabalho-1-p3/src/main.rs
@@ -1,4 +1,10 @@
-use opencv::{highgui, imgproc::gaussian_blur, prelude::*, videoio, Result};
+use opencv::{
+    core::Size_,
+    highgui::{self, ButtonCallback, QtButtonTypes, QT_PUSH_BUTTON},
+    imgproc::{canny, gaussian_blur},
+    prelude::*,
+    videoio, Result,
+};
 
 fn main() -> Result<()> {
     let mut cam =
@@ -6,7 +12,7 @@ fn main() -> Result<()> {
 
     let mut frame = Mat::default();
 
-    let window = highgui::named_window("window", highgui::WINDOW_FULLSCREEN)?;
+    highgui::named_window("window", highgui::WINDOW_FULLSCREEN)?;
     let mut kernel_size = 1;
     highgui::create_trackbar(
         "Gaussian",
@@ -16,13 +22,17 @@ fn main() -> Result<()> {
         None,
     )?;
 
+    // highgui::create_button("Fuzzy", None, 0, false)?;
+
     loop {
         cam.read(&mut frame)
             .expect("Should be able to acquire new frame!");
 
         let mut frame_out = Mat::default();
 
-        apply_gaussian(&frame, &mut frame_out, kernel_size)?;
+        // apply_gaussian(&frame, &mut frame_out, kernel_size)?;
+
+        apply_canny(&frame, &mut frame_out)?;
 
         highgui::imshow("window", &frame_out)?;
 
@@ -36,6 +46,22 @@ fn main() -> Result<()> {
     Ok(())
 }
 
+fn apply_canny(frame: &Mat, frame_out: &mut Mat) -> Result<(), opencv::Error> {
+    gaussian_blur(
+        frame,
+        frame_out,
+        Size_ {
+            width: 5,
+            height: 5,
+        },
+        0.0,
+        0.0,
+        0,
+    )?;
+    canny(&frame_out.clone(), frame_out, 40.0, 100.0, 3, false)?;
+    Ok(())
+}
+
 fn apply_gaussian(frame: &Mat, frame_out: &mut Mat, kernel_size: i32) -> Result<(), opencv::Error> {
     let mut copied_kernel = kernel_size;
 
@@ -46,7 +72,7 @@ fn apply_gaussian(frame: &Mat, frame_out: &mut Mat, kernel_size: i32) -> Result<
     gaussian_blur(
         frame,
         frame_out,
-        opencv::core::Size_ {
+        Size_ {
             width: copied_kernel,
             height: copied_kernel,
         },
